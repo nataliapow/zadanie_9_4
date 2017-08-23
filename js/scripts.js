dfcdcaf07f76baec340efb399b4b6595e9d4ea03
Natalia <n.powalska@gmail.com>
2017-08-23 14:24:35 +0200

    Add function call

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 function drawTree (n) {
 	for (var i=0; i < n; i++) {
-		var star = " ";
+		var star = "";
 		for (var j = 0; j <= i; j++) {
-			var star = star += ' *';
+			star += '*';
 		}
 		console.log(star);
 	}
 
 }
+drawTree (10);
